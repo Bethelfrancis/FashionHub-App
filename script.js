b84b88151460ffd8384cecdfb2e5e0f995f62650
Bethelkid <bethelndegonekwu@gmail.com>
2024-09-15 21:10:02 +0100

    Initial Commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const productContainer = document.getElementById('product-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 const hamburger = document.querySelector('.hambuger');
-const mobileNav = document.querySelector('.mobile-nav-links');
-const body = document.getElementById('body');
+const mobileNav = document.querySelector('.nav-links');
 const aboutSection = document.querySelector('.about-sec');
 const navbar = document.querySelector('.nav-bar');
+const mobileBtn = document.querySelectorAll('.nav-link-btn');
+const cartIcon = document.getElementById('cart-icon');
+const cartContainer = document.getElementById('cart-container');
+const closeCartBtn = document.querySelector('.cart-close');
+const pushContainer = document.getElementById('push-container');
 
 let lastScrollTop = 0;
 
@@ -30,10 +34,30 @@ const checkVisibility = () => {
 window.addEventListener('scroll', checkVisibility);
 checkVisibility();
 
+cartIcon.addEventListener('click', () => {
+  cartContainer.style.top = '0'
+})
+
+// document.addEventListener('click', (e) => {
+//   if (!cartContainer.contains(e.target) && !cartIcon.contains(e.target)) {
+//     cartContainer.style.top = '-100rem'
+//   }
+// });
+
+closeCartBtn.addEventListener('click', () => {
+  cartContainer.style.top = '-100rem'
+})
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('open');
   mobileNav.classList.toggle('open');
-  body.classList.toggle('body')
+});
+
+mobileBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    hamburger.classList.remove('open');
+    mobileNav.classList.remove('open');
+  });
 });
 
 let startingIndex = 0;
@@ -72,14 +96,47 @@ const displayProducts = (product) => {
             alt="${title} img"
             id="category-img"
         />
-        <button id="pro-shop-now">Shop Now</button>
-        <h3>${title}</h3>
+        <button class="pro-shop-now">Shop Now</button>
+        <h3>${title.length > 20 ? title.slice(0, 20) + '...' : title}</h3>
         <div class="categorys-price">
             <h4><span id="category-price">Price:</span>$${price}</h4>
         </div>
     </div>
   `;
   });
+
+  const shopNowBtn = document.querySelectorAll('.pro-shop-now');
+
+  shopNowBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      const productId = Number(e.target.closest('.category-product').id);
+      cart.addItems(productId, productDataArr);
+    })
+  })
 };
 
 loadMoreBtn.addEventListener('click', fetchMoreProducts);
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.tax = 5;
+    this.total = 0
+  }
+
+  addItems(id, products) {
+    const product = products.find(item => item.id === id);
+    const { image, title, price } = product;
+    this.items.push(product);
+
+    pushContainer.innerHTML += `
+      <div class="product-display" id="${id}">
+          <img src="${image}" alt="${title}-image">
+          <h3>${title}</h3>
+          <h4>${price}</h4>
+      </div>
+    `;
+  }
+}
+
+const cart = new ShoppingCart;
